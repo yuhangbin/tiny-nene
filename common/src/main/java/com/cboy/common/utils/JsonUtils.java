9535c382f033cacee958b6bca8b9e310a6933aad
CharlieYu <yuhangbinccc@gmail.com>
2024-04-21 20:23:14 +0800

    feat: JsonUtils

---
+++ common/src/main/java/com/cboy/common/utils/JsonUtils.java
@@ -0,0 +1,37 @@
+package com.cboy.common.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+
+    static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+//        objectMapper.configure();
+    }
+
+    public static <T> String toJson(T object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Throwable e) {
+            return "";
+        }
+    }
+
+    public static <T> T fromJson(String json, TypeReference<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+}
