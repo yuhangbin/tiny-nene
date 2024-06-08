07a4f24ba0a148c3921fded6e10284f293c49691
CharlieYu <yuhangbinccc@gmail.com>
2024-06-08 20:19:57 +0800

    feat: TcpClient

---
+++ connection/src/main/java/com/cboy/connection/client/ClientHandler.java
@@ -0,0 +1,32 @@
+package com.cboy.connection.client;
+
+import com.cboy.common.pojo.NeneMsg;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ChannelHandler.Sharable
+public class ClientHandler extends SimpleChannelInboundHandler<NeneMsg> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, NeneMsg msg) throws Exception {
+        log.info("[ClientHandler:channelRead0] msg:{}", msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
--- connection/src/main/java/com/cboy/connection/client/TcpClient.java
+++ connection/src/main/java/com/cboy/connection/client/TcpClient.java
@@ -1,4 +1,68 @@
 package com.cboy.connection.client;
 
+
+import com.cboy.common.pojo.NeneMsg;
+import com.cboy.common.utils.JsonUtils;
+import com.cboy.connection.codec.NeneDecoder;
+import com.cboy.connection.codec.NeneEncoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import java.net.InetSocketAddress;
+import java.util.UUID;
+
+@Slf4j
 public class TcpClient {
+
+    private final String host;
+    private final int port;
+
+    private final EventLoopGroup executors = new NioEventLoopGroup();
+
+    ChannelFuture channelFuture;
+
+    public TcpClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() {
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(executors)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NeneDecoder());
+                            ch.pipeline().addLast(new ClientHandler());
+                            ch.pipeline().addLast(new NeneEncoder());
+                        }
+                    });
+            channelFuture = bootstrap.connect().sync();
+            NeneMsg<?> req = new NeneMsg<>();
+            req.setMsgId(UUID.randomUUID().toString());
+            channelFuture.channel().writeAndFlush(req);
+            log.info("[TcpClient:start] req:{}", JsonUtils.toJson(req));
+        } catch (Throwable e) {
+            log.error("[TcpClient:start] e:{}", ExceptionUtils.getStackTrace(e));
+        }
+
+    }
+
+    public void shutdown() {
+        try {
+            executors.shutdownGracefully().syncUninterruptibly();
+        } catch (Throwable e) {
+            log.error("[TcpClient:shutdown] e:{}", ExceptionUtils.getStackTrace(e));
+        }
+    }
 }
